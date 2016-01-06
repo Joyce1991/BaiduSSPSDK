4b15d7ea6c254d68f2f85804c4fc955ee8fde7d7
jalen_kyx <joyceit1991@gmail.com>
2016-01-06 20:05:10 +0800

    百度广告sdk初始化

---
+++ app/src/main/java/com/jalenzhang/baidusspsdk/JoActivity.java
@@ -0,0 +1,20 @@
+package com.jalenzhang.baidusspsdk;
+
+import android.app.Activity;
+
+/**
+ * <p>Title: 启动页面</p>
+ * <p/>
+ * <p>Description: </p>
+ * 百度广告配置
+ * <p>Copyright: Copyright (c) 2010</p>
+ * <p>Company: kuaiyouxi.com</p>
+ * <p>Created on 2016/1/6.
+ *
+ * @author jalen-pc
+ * @version 1.1.1
+ */
+
+public class JoActivity extends Activity {
+
+}
